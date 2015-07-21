b8deaf8f5c5ecaff5c3fba047abdbccb2b350b61
Alessandro Fazzi <alessandro.fazzi@welaika.com>
2015-07-21 12:05:07 +0200

    fix #2

--- install.sh
+++ install.sh
@@ -4,6 +4,10 @@ BACKUP_DIR="$(pwd)/backup"
 SUBLIME_FILES="$(pwd)/sublime/User"
 SUBLIME_DEST_DIR="${HOME}/Library/Application Support/Sublime Text 3/Packages/User"
 
+function warn() {
+  echo "$(tput bold)$(tput setaf 3)  *** ${1}$(tput sgr0)"
+}
+
 function backup_sublime() {
 	[[ -d "$SUBLIME_DEST_DIR" && ! -L "$SUBLIME_DEST_DIR" ]] || return;
 	backup="${BACKUP_DIR}/sublime/User"
